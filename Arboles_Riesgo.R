40fadb9135c7f1cc1832144a39b320e11d57fe4b
ramonhidalgo <ramon@uma.es>
2021-06-02 00:57:08 +0200

    Add files via upload

---
+++ Arboles_Riesgo.R
@@ -0,0 +1,28 @@
+# Árboles de decisión 
+
+
+library(readxl)
+library(expss)
+
+
+dir1 <- "D:/OneDrive - Universidad de Málaga/Usuarios/Martina/IHQ_Prosigna"
+
+setwd(dir1)
+dat <- read_excel("IHQ-Prosigna_1521.xls")
+
+
+library(rpart)        
+library(rpart.plot)   
+library(caret)        
+
+library(dplyr)  # Manipulación de datos
+
+attach(dat)
+datos_red <- dat %>%
+  select(c(RIESGO, KI67, RE, RP, GRADO)) %>%
+  mutate(GRADO   = factor(GRADO, levels = c(1,2,3), labels= c('Bajo', 'Intermedio','Alto')))  %>%
+  na.omit()  
+
+
+arbol1 <- rpart(formula = RIESGO ~., data=datos_red, method='class')
+rpart.plot(arbol1)
